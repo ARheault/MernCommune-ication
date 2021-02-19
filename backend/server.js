ade8408211f2aeb42dd528714d926964818df5e3
AlexRheault <alexdrhlt@gmail.com>
2021-02-19 15:32:38 -0800

    Fixes

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
-})
+});
 
 // API routes
 
